21a853406263773afca02f4838850411d5527a96
Flynn Lundeen <fsl8307@g.rit.edu>
2022-11-28 12:31:27 -0500

    work on proposal

---
+++ project3/js/main.js
@@ -0,0 +1,9 @@
+class piece{
+    constructor(moveset,x,y,name, team){
+        this.moveset = moveset;
+        this.x = x;
+        this.y = y;
+        this.name = name;
+        this.team = team;
+    }
+}
